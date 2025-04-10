fe941ddaf288716bb18a51691106c2d403936ea9
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-04-10 23:16:52 +0530

    update and delete user

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = mongoose.Schema({
   email: {
     type: String,
   },
-  Password: {
+  password: {
     type: String,
   },
 });
--- src/app.js
+++ src/app.js
@@ -2,15 +2,67 @@ const express = require("express");
 const app = express();
 
 const connectDb = require("./config/database");
+const User = require("../models/user");
+
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.send("Data added successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const users = await User.find({ email: userEmail });
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find({});
+    res.send(user);
+  } catch (error) {
+    console.log("We found an error ", error);
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId
+  try {
+  const user = await User.findByIdAndDelete(userId)
+    res.send("User deleter successfullyy");
+  } catch (error) {
+    console.log("We found an error ", error);
+  }
+});
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId
+  const data= req.body
+  try {
+  const user = await User.findByIdAndUpdate({_id:userId},data)
+    res.send("User deleter successfullyy");
+  } catch (error) {
+    console.log("We found an error ", error);
+  }
+});
 
 connectDb()
   .then(() => {
     console.log("database connected successfully");
-    app.listen(() => {
-      console.log("server on listening on port no. 7777");
+    app.listen(3000, () => {
+      console.log("server on listening on port no. 3000");
     });
   })
   .catch((err) => {
-
-    console.log("something went wrong" , err)
+    console.log("something went wrong", err);
   });

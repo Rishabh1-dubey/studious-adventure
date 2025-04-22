afbdd49ac753420c37607b74f9529a3da0a9ffa8
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-04-22 22:25:02 +0530

    adding user models

--- models/user.js
+++ models/user.js
@@ -3,17 +3,44 @@ const { default: mongoose } = require("mongoose");
 const userSchema = mongoose.Schema({
   firstName: {
     type: String,
+    required: true,
   },
   lastName: {
     type: String,
   },
   email: {
     type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    trim: true,
   },
   password: {
     type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    validate(value) {
+      if (!["male", "female", "others"].includes(value)){
+        throw new Error("gender data is not valid")
+      }
+    },
+  },
+  age: {
+    type: String,
+  },
+  photoUrl: {
+    type: String,
+  },
+  about: {
+    type: String,
+    default: "this is all about  about us section",
+  },
+  skills: {
+    type: [String],
   },
 });
 
-const User = mongoose.model("User" , userSchema)
- module.exports = User;
+const User = mongoose.model("User", userSchema);
+module.exports = User;
